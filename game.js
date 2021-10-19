aab0d20ca2ccbe228fa1f1c17f61a90abe55ea5f
Shujaat Azim <shujaat.azim@gmail.com>
2021-10-19 00:24:14 -0400

    Added more features; random rectangle height, infinite spawning, lose scene, and a score.

--- game.js
+++ game.js
@@ -1,41 +1,66 @@
-kaboom({
-  global: true
-});
+kaboom()
+loadSprite("ghost", "ghost.png");
 
-loadSprite("ghost", "ghost.png")
-
-const ghost = add([
+scene("game", () => {
+  const ghost = add([
     sprite("ghost"),
     pos(80, 40),
     area(),
     body(),
-]);
+  ]);
 
-add([
+  add([
     rect(width(), 48),
     pos(0, height() - 48),
     outline(4),
     area(),
     solid(),
     color(127, 200, 255),
-]);
+  ]);
 
-add([
-  rect(48, 64),
-  area(),
-  outline(4),
-  pos(width(), height() - 48),
-  origin("botleft"),
-  color(255, 180, 255),
-  move(LEFT, 240),
-  "tree"
-]);
+  loop(1, () => {
+    add([
+      rect(48, rand(24, 64)),
+      area(),
+      outline(4),
+      pos(width(), height() - 48),
+      origin("botleft"),
+      color(255, 180, 255),
+      move(LEFT, 240),
+      "tree"
+    ]);
 
-keyPress("space", () => {
-  if (ghost.grounded()) ghost.jump();
+  });
+
+  keyPress("space", () => {
+    if (ghost.grounded()) ghost.jump();
+  });
+
+  ghost.collides("tree", () => {
+    addKaboom(ghost.pos);
+    shake();
+    go("lose");
+  });
+
+  let score = 0;
+  const scoreLabel = add([
+    text(score),
+    pos(24, 24)
+  ]);
+
+  action(() => {
+    score++;
+    scoreLabel.text = score;
+  });
+
 });
 
-ghost.collides("tree", () => {
-  addKaboom(ghost.pos);
-  shake();
+go("game")
+
+scene("lose", () => {
+  add([
+    text("Game Over"),
+    pos(center()),
+    origin("center"),
+  ]);
 });
